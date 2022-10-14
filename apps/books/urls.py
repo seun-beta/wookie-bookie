6d1489206a1b5c6d08c52d69b44ab4ddee214b77
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-10-14 01:48:32 +0100

    [UPD] Rename endpoint routes
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from apps.books.views import BookIDView, BookListView, CreateBookView
+from apps.books.views import BookListView, BookView, CreateBookView
 
 urlpatterns = [
     path(
-        "books/list-books",
+        "books/non-auth-books/",
         BookListView.as_view(),
-        name="unautheticated_book_list",
+        name="non_auth_book_list",
     ),
-    path("books/books", CreateBookView.as_view(), name="create_book"),
-    path("books/book-id", BookIDView.as_view(), name="book_id"),
+    path("books/books/", CreateBookView.as_view(), name="create_book"),
+    path("books/books/<str:id>/", BookView.as_view(), name="book"),
 ]
