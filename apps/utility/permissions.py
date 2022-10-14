b28aef4d0d0dcd110b1771ff15dcf5fa69146268
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-10-14 01:49:31 +0100

    [UPD] Fix permission error
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- apps/utility/permissions.py
+++ apps/utility/permissions.py
@@ -4,5 +4,5 @@ from rest_framework.permissions import BasePermission
 class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
 
-        if obj.user == request.user:
+        if obj.author == request.user:
             return True
