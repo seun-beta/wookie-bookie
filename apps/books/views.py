90f9d2e11204b90430165e62341628a893a765c6
seun-beta <adegokeseunfunmi1999@gmail.com>
2022-10-15 12:29:58 +0100

    [UPD] Implement pagination and  custom filter
    Signed-off-by: seun-beta <adegokeseunfunmi1999@gmail.com>

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import (
     ListAPIView,
     ListCreateAPIView,
@@ -8,6 +9,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from apps.books.models import Book
 from apps.books.serializers import BookListSerializer, BookSerializer
+from apps.utility.filters import BookFilter
+from apps.utility.pagination import BookPagination
 from apps.utility.permissions import IsAuthor
 
 
@@ -15,17 +18,27 @@ class BookListView(ListAPIView):
     serializer_class = BookListSerializer
     queryset = Book.objects.all().select_related()
     permission_classes = []
+    pagination_class = BookPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
 
 
-class CreateBookView(ListCreateAPIView):
+class ListCreateBookView(ListCreateAPIView):
     serializer_class = BookSerializer
     queryset = Book.objects.all().select_related()
     parser_classes = [MultiPartParser, FormParser]
+    pagination_class = BookPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
 
-    def get_queryset(self):
+    def get_queryset(self) -> Book:
+        if getattr(self, "swagger_fake_view", False):
+            # queryset just for schema generation metadata
+            return Book.objects.none()
+
         return self.queryset.filter(author=self.request.user).select_related()
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer: dict) -> dict:
         return serializer.save(author=self.request.user)
 
 
